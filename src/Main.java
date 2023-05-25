89ff10f783ce515e43b64bd9f836d519f09841df
Minsa111 <dragonriderrgt@gmail.com>
2023-05-25 08:11:52 +0700

    done

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        BinaryTree tree = new BinaryTree();
+
+        tree.NewNode( 20 );
+        tree.NewNode( 2 );
+        tree.NewNode( 25 );
+        tree.NewNode( 37 );
+        tree.NewNode( 16 );
+
+        System.out.println("\nIn Order\t: ");
+        tree.inOrder( tree.root );
+        System.out.println("\nPre Order\t: ");
+        tree.preOrder( tree.root );
+        System.out.println("\nPost Order\t:");
+        tree.postOrder( tree.root );
+    }
+}
